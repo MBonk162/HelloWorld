22676ac1e56070112d8e83d808d760127729667e
mbonkowski <mbonkowski@bidtellect.com>
2021-10-08 16:24:32 -0400

    First commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any())
+            {
+                Console.WriteLine($"Hello {args.First()}");
+            }
+            else
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
